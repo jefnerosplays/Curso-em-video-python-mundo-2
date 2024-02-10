b2eafc226d1e936fe486efdc9b68dc2a2a8a44d4
jefnerosplays <jefersongeek123@hotmail.com>
2024-02-10 14:18:11 -0300

    Qual é o maior?

---
+++ Disafios/#038.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+j = int(input('Digite outro número: '))
+if n > j :
+    print('O primeiro número é o maior')
+elif n < j :
+    print('O segundo número é o maior')
+elif n == j :
+    print('Os dois números são iguais!!')
