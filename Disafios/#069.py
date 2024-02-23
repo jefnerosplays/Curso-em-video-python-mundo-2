dc49da387dc9d26093f295d5210ad80056038b1d
jefnerosplays <jefersongeek123@hotmail.com>
2024-02-23 19:51:07 -0300

    23/02

--- Disafios/#069.py
+++ Disafios/#069.py
@@ -1,6 +1,8 @@
 q = 0
 c = 0
-maisde18=0
+maisde18 = 0
+menosde20 = 0
+homems = 0
 while c == 0:
     sexo = input('Qual é seu genero? [M|F]: ').upper()
     idade = int(input('Qual é a sua idade? '))
@@ -8,6 +10,13 @@ while c == 0:
     print('~'*30)
     if n == 'N':
         c = 1
-if sexo == 'M'and idade > 18:
-    maisde18 += 1
-print(maisde18)
+    if idade > 18:
+        maisde18 = maisde18 + 1
+    if sexo == 'M':
+        homems += 1
+    if sexo == 'F' and idade < 20:
+        menosde20 += 1
+
+print(f'->A quantidade de pessoas com mais de 18 anos é {maisde18}')
+print(f'->Foram registrados {homems} homens')
+print(f'->Foram registradas {menosde20} mulheres com menos de 20 anos')
