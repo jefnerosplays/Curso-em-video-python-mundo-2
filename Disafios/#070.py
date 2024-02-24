d5516bf22e317dc982a16bcb3a39a511b2c51969
jefnerosplays <jefersongeek123@hotmail.com>
2024-02-24 13:52:38 -0300

    24/02

--- Disafios/#070.py
+++ Disafios/#070.py
@@ -1,1 +1,20 @@
-
+print('='*30)
+print('MERCADÃO DO JEFÃO')
+print('='*30)
+print('~'*30)
+maisde1000 = 0
+total = 0
+while True:
+    nome = input('Produto: ')
+    preco = float(input('Preço: R$'))
+    escolha = input('Quer continuar? [S|N]').upper()
+    print('-'*30)
+    print('~'*30)
+    
+    if preco > 1000.0:
+        maisde1000 += 1
+    total += preco
+    if escolha == 'N':
+        break
+print(f'A quantidade de produtos que custam mais de 1000 R$ é {maisde1000}')
+print(f'A soma de todos os preços é de {total} R$')
